52aed5156519ea73e708825944c5f469bdf0f629
Romain TRUCHI <romain.truchi.06@gmail.com>
2018-08-14 02:45:36 +0200

    Background: fetching files

--- controllers/Background.js
+++ controllers/Background.js
@@ -23,13 +23,18 @@ class Background {
     const id  = navigation.tabId
     const url = navigation.url
 
-    const files = this.shrome.files(url, this.config.theme)
-    chrome.tabs.sendMessage(id, {
-      files: {
-        js : files.js,
-        css: files.css
-      }
-    })
+    const files = this.shrome.files(url, this.config.theme, this.config.local)
+    const base  = (file) => this.config.local
+      ? this.shrome.sanitized.local + file
+      : Request.githubFileUrl(this.config.user, this.config.repo, this.config.sha, file)
+
+    Promise.all([
+      Promise.all(files.js .map(js  => Request.get(base(js )))),
+      Promise.all(files.css.map(css => Request.get(base(css))))
+    ])
+      .then(([ js, css ]) => {
+        chrome.tabs.sendMessage(id, { files: { js, css } })
+      })
   }
 }
 
