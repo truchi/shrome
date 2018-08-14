81e930c403ae3a601274beb7b5155cf864eaff54
Romain TRUCHI <romain.truchi.06@gmail.com>
2018-08-14 04:13:50 +0200

    clean

--- background.js
+++ background.js
@@ -2,20 +2,4 @@
 
 const background = new Background()
 
-chrome.windows.getAll({ populate: true }, (windows) => {
-  console.log(windows)
-
-  for (let window of windows) {
-    for (let tab of window.tabs) {
-      console.log(window.id, tab.id, tab.url)
-
-      if (!tab.url.startsWith('chrome://')) {
-        chrome.tabs.insertCSS(tab.id, {
-          code: '* {color:red}'
-        })
-      }
-    }
-  }
-})
-
 chrome.webNavigation.onCompleted.addListener(background.onCompleted.bind(background))
