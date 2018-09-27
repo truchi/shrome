20e753568180c30b59c07edb87131ae81da8af32
Romain TRUCHI <romain.truchi.06@gmail.com>
2018-09-27 16:26:33 +0200

    Github::discover: find all repos with 'shrome-themes' topics

--- controllers/Github.js
+++ controllers/Github.js
@@ -26,7 +26,7 @@ export default class Github extends BaseRequest {
 
   _url(user, repo) {
     return new Promise((resolve, reject) =>
-      this.file(`https://api.github.com/repos/${ user }/${ repo }/commits`)
+      this.file(`${ Github.apiUrl }/repos/${ user }/${ repo }/commits`)
         .then((commits) => (commits = JSON.parse(commits)) ||
           commits.length
             ? resolve(`https://rawgit.com/${ user }/${ repo }/${ commits[0].sha }/`)
@@ -34,5 +34,60 @@ export default class Github extends BaseRequest {
         )
         .catch(() => reject(`Invalid user/repo: ${ this._user }/${ this._repo }`))
     )
+  }
+
+  static discover() {
+    return new Promise((resolve, reject) => {
+      const ret = []
+      const req = new BaseRequest()
+
+      req.file(`${ Github.apiUrl }/search/repositories?q=topic:shrome-themes`)
+        .then((response) => {
+          let promises = []
+          response = JSON.parse(response)
+
+          response.items.forEach(repo => {
+            const info = {
+              name: repo.name,
+              desc: repo.description || '',
+              url : repo.html_url,
+              head: {
+                branch: repo.default_branch,
+              },
+              user: {
+                name: repo.owner.login,
+                url : repo.owner.html_url,
+                img : repo.owner.avatar_url
+              }
+            }
+
+            promises.push(
+              new Promise((resolve, reject) => {
+                req.file(`${ repo.url }/commits/${ info.head.branch }`)
+                  .then(commit => {
+                    commit = JSON.parse(commit)
+
+                    info.head.sha  = commit.sha
+                    info.head.url  = commit.html_url
+                    info.head.date = commit.commit.committer.date
+
+                    ret.push(info)
+                    resolve()
+                  })
+                  .catch(reject)
+              })
+            )
+
+            Promise.all(promises)
+              .then(() => resolve(ret))
+              .catch(reject)
+          })
+        })
+        .catch(reject)
+    })
+  }
+
+  static get apiUrl() {
+    return 'https://api.github.com'
   }
 }
--- options.js
+++ options.js
@@ -1,6 +1,12 @@
 import Source  from './views/Source.js'
 import Config  from './views/Config.js'
 import Options from './controllers/Options.js'
+import Github  from './controllers/Github.js'
+
+Github.discover()
+  .then(repos => {
+    console.log(repos)
+  })
 
 const $source = document.getElementById('source')
 const $config = document.getElementById('config')
