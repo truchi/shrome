ab779ef01f437d1b5e66986e5d73c1c9e594cfb9
Romain TRUCHI <romain.truchi.06@gmail.com>
2018-10-22 18:40:56 +0200

    SearchView: a start

--- views/SearchView.js
+++ views/SearchView.js
@@ -1,11 +1,50 @@
-import View from './View.js'
+import View           from './View.js'
+import SearchItemView from './SearchItemView.js'
 
 export default class SearchView extends View {
   constructor(_$) {
     super(_$)
+
+    this._$$ = {
+      $items: []
+    }
   }
 
-  render(data) {
-    console.log(data)
+  _preRender() {
+    return this
+  }
+
+  _preRender() {
+    return this
+  }
+
+  render(repos) {
+    this._preRender()
+    this._$.innerHTML = repos ? this._render(repos) : ''
+    this._postRender(repos)
+
+    return this
+  }
+
+  _postRender(repos) {
+    this._$$ = {
+      items: []
+    }
+
+    Array.from(this._$.querySelectorAll('.search-item-container'))
+      .forEach(($, i) =>
+        this._$$.items
+          .push((new SearchItemView($)).render(repos[i]))
+      )
+
+    return this
+  }
+
+  _render(repos) {
+    return `
+      ${ repos.map((repo, i) => `
+        <div class="search-item-container ${ i }"></div>
+      `) }
+    `
   }
 }
